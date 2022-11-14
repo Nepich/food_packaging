8df37e5050dd8641bad4cbb7e238a8b356ed9809
Dmitriy <nepich@gmail.com>
2022-11-14 13:31:52 +0300

    Удалено лишнее

--- food_packaging/food_packaging/celery.py
+++ food_packaging/food_packaging/celery.py
@@ -6,8 +6,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'food_packaging.settings')
 app = Celery('food_packaging')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
-
-
-@app.task(bind=True)
-def debug_task(self):
-    print(f'Request: {self.request!r}')
